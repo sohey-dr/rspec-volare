fe7c3d9ba1d0c9e48ab7b9319c629ce34d8f1fcd
sohey-dr <heyso1209@gmail.com>
2021-07-03 22:40:29 +0900

    Add fizz_buzz

--- lib/user.rb
+++ lib/user.rb
@@ -13,4 +13,16 @@ class User
       "僕は#{name}です"
     end
   end
+
+  def fizz_buzz
+    if age % 15 == 0
+      "fizz buzz"
+    elsif age % 3 == 0
+      "fizz"
+    elsif age % 5 == 0
+      "buzz"
+    else
+      age.to_s
+    end
+  end
 end
