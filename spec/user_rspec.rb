23750cee05ec5127483c83603e83cb9496be08ef
sohey-dr <heyso1209@gmail.com>
2021-07-03 21:42:01 +0900

    Add subject

--- spec/user_rspec.rb
+++ spec/user_rspec.rb
@@ -2,19 +2,20 @@ require 'spec_helper'
 
 RSpec.describe User do
   describe "#greet" do
+    subject { user.greet } 
     let(:user) {User.new(name: "たろう", age: age)}
 
     context "12歳以下の時" do
       let(:age) {12}
       it "ひらがなで答えること" do
-        expect(user.greet).to  eq "ぼくはたろうだよ"
+        is_expected.to  eq "ぼくはたろうだよ"
       end
     end
 
     context "13歳より大きい時" do
       let(:age) {14}
       it "漢字で答えること" do
-        expect(user.greet).to  eq "僕はたろうです"
+        is_expected.to  eq "僕はたろうです"
       end
     end
   end
